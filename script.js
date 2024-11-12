2e2612e6e5b187be40bda358ba3fe3acc25b58ac
nguyendang16 <nguyendk0106@gmail.com>
2024-11-12 15:29:05 +0700

    a

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const buttonsContainer = document.querySelector(".buttons");
 const yesButton = document.querySelector(".btn--yes");
 const noButton = document.querySelector(".btn--no");
 const catImg = document.querySelector(".cat-img");
+const formElement = document.querySelector(".form");
 
 const MAX_IMAGES = 5;
 
@@ -27,14 +28,52 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! Hẹn gặp em vào thứ 4 ^^ ";
+  // Ẩn tiêu đề và các nút
+  titleElement.classList.add("hidden");
   buttonsContainer.classList.add("hidden");
-  changeImage("yes");
+
+  // Hiển thị biểu mẫu
+  formElement.classList.remove("hidden");
+
+  // Thay đổi hình ảnh thành cat-0.jpg khi biểu mẫu hiển thị
+  catImg.src = "img/cat-0.jpg";
+}
+
+formElement.addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(formElement);
+
+  fetch("/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(function () {
+      // Hiển thị thông điệp cuối cùng
+      titleElement.innerHTML = "Yayyy!! Hẹn gặp em vào tối nay ^^";
+      titleElement.classList.remove("hidden");
+
+      // Ẩn biểu mẫu
+      formElement.classList.add("hidden");
+
+      // Thay đổi hình ảnh khi nhấn "Yes" để gửi thông điệp cuối cùng
+      changeImage("yes");
+    })
+    .catch(function (error) {
+      console.error("Form submission error:", error);
+    });
 }
 
 function resizeYesButton() {
   const computedStyle = window.getComputedStyle(yesButton);
-  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const fontSize = parseFloat(
+    computedStyle.getPropertyValue("font-size")
+  );
   const newFontSize = fontSize * 1.6;
 
   yesButton.style.fontSize = `${newFontSize}px`;
@@ -55,7 +94,7 @@ function generateMessage(noCount) {
 }
 
 function changeImage(image) {
-  catImg.src = `/img/cat-${image}.jpg`;
+  catImg.src = `img/cat-${image}.jpg`;
 }
 
 function updateNoButtonText() {
